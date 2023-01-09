174230803a578e6a10724ab7a2c6c0d5ba589bb1
mukesh <mukese0794@gmail.com>
2023-01-09 12:27:05 +0530

    images

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Simple Web Application is UP');
-  console.log('Hi')
-});
+app.use(express.static('public'))
 
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Simple Web Application running on port ${port}`));
+
+
+//http://localhost:5000/shiveJee.jpg
